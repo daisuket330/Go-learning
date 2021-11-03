7d6483417c1375ea51b167025e175bdfe4925d1e
daisuket330 <fasuke322@gmail.com>
2021-11-02 23:08:04 -0400

    some beginner go functions

--- main.go
+++ main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
+func add(x int, y int) int {
+	return x + y
+}
 func main() {
 	fmt.Println("Hello World")
 	fmt.Println("My name is Treveon")
 	fmt.Println("The time is", time.Now())
+	fmt.Println("My favorite number is", rand.Intn(10))
+	fmt.Println(add(33, 36), ",nice")
 }
